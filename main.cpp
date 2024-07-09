819b392de47c19b0c3af6224cd738ab05bf99695
David_Wideside <a1234@Davids-MacBook-Air.local>
2024-07-09 16:33:43 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <QApplication>
+#include "login_dialog.h"
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    LoginDialog loginDialog;
+    if (loginDialog.exec() == QDialog::Accepted) {
+        MainWindow mainWindow;
+        mainWindow.show();
+        return app.exec();
+    }
+
+    return 0;
+}
