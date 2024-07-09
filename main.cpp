f957ab633c6e3ee9dc4a3ff56bbe431c1bc9d411
David0Jiang <2041524750@qq.com>
2024-07-09 16:55:51 +0800

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,24 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     LoginDialog loginDialog;
+
+
+
     if (loginDialog.exec() == QDialog::Accepted) {
         MainWindow mainWindow;
         mainWindow.show();
         return app.exec();
     }
 
+
+
+
     return 0;
+
+
+
+
 }
+
+
+
